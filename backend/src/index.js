cd53c4dc04bdc40caf42de84c2de5fcebd9cb4c1
Matheus Todao <todaomatheus@hotmail.com>
2021-10-06 16:15:49 -0300

    Backend

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('express-async-errors');
+
+const cors = require('./middlewares/cors');
+const errorHandler = require('./middlewares/errorHandler');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors);
+
+app.use(routes);
+
+app.use(errorHandler);
+
+const port = 3001;
+app.listen(port, () => console.log(`🔥 Server started at http://localhost:${port}`));
